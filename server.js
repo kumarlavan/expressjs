6ebb9c9d5143576406674d9853dd036fe2be70ce
kumarlavan <330mekalalavankumar@gmail.com>
2025-03-03 02:54:15 +0530

    add products api

--- server.js
+++ server.js
@@ -6,5 +6,16 @@ app.use(express.json())
 app.get("/item",(req,res)=>{
     res.status(200).json({message:"Data Send Successfully"})
 })
+app.get("/products",(req,res)=>{
+    const data= fetch("https://api.github.com/users")
+    data.then((response)=>{
+       const user= response.json()
+       user.then((userdata)=>{
+        res.status(200).json({message:"user data",userdata})
+       })
+    }).catch((err)=>{
+        res.status(500).json({message:"Internal server error"})
+    })
+})
 
 module.exports=app
